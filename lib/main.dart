36f994120f8c25fc09bcb4d92126814052505242
Phu Ngo Minh <ngominhphu.qtkd@gmail.com>
2025-07-10 08:16:41 +0700

    default location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
+  GoogleMapController? mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  LatLng _center = const LatLng(21.0285, 105.8542); // default location Hà Nội
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
+  Future<void> _determinePosition() async {
+    await Permission.location.request(); // request location access
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) return;
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) return;
+    }
+    Position position = await Geolocator.getCurrentPosition();
+
+    setState(() {
+      _center = LatLng(position.latitude, position.longitude);
+    });
+
+    mapController?.animateCamera(
+      CameraUpdate.newCameraPosition(CameraPosition(target: _center, zoom: 14)),
+    );
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -24,12 +53,14 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.green[700]),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.green[50]),
       home: Scaffold(
         appBar: AppBar(title: const Text('Maps Sample App'), elevation: 2),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
         ),
       ),
     );
